c1acfa1b71919e98324278c9b4db2bf9f75931ad
Kritias <50093609+ElPlaguister@users.noreply.github.com>
2021-08-28 01:44:14 +0900

    separate js

---
+++ Darkmode.js
@@ -0,0 +1,38 @@
+var Links = {
+    setColor: function(color) {
+        var alist = document.querySelectorAll('a');
+        var i = 0;
+        while(i < alist.length) {
+            alist[i].style.color = color;
+            i += 1;
+        }
+    }
+}
+var Body = {
+    setColor: function(color) {
+        document.querySelector('body').style.color = color;
+    },
+    setBackgroundColor: function(color) {
+        document.querySelector('body').style.backgroundColor = color;
+    }
+}
+function DarkmodeHandler(self) {
+    var target = document.querySelector('body');
+    var acolor, nextValue, backColor, fontColor, acolor;
+    if(self.value == 'dark') {
+        nextValue = 'light';
+        backColor = 'black';
+        fontColor = 'white';
+        acolor = 'powderblue';
+    }
+    else {
+        nextValue = 'dark';
+        backColor = 'white';
+        fontColor = 'black';
+        acolor = 'black';
+    }
+    self.value = nextValue;
+    Body.setBackgroundColor(backColor);
+    Body.setColor(fontColor);
+    Links.setColor(acolor);
+}
